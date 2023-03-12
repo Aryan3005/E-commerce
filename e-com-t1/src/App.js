8a48e376a84412241c46880cba3697e712a0a4e0
Kaival <87368466+Kaival1212@users.noreply.github.com>
2023-03-12 19:55:39 +0000

    firebase API
    Just for reading the data

--- e-com-t1/src/App.js
+++ e-com-t1/src/App.js
@@ -2,10 +2,33 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Item from './Components/Items';
+import { useState } from 'react';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 import NavBarComponent from './Components/NavBar'
+import { doc, getDoc } from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDFNgbvMK16101ygeQksKe7YTw8Wc78bng",
+  authDomain: "e-com-ti.firebaseapp.com",
+  databaseURL: "https://e-com-ti-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "e-com-ti",
+  storageBucket: "e-com-ti.appspot.com",
+  messagingSenderId: "740384603849",
+  appId: "1:740384603849:web:9d471bb5c5fa115a55d718",
+  measurementId: "G-6K2MNFTDHS"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
 
 
 function App() {
+
   return(
     <BrowserRouter>
       <Routes>
@@ -28,6 +51,14 @@ function LandingPage()
   const items = Array.from({ length: n }, (_, index) => (
     <Item key={index} />
   ));
+
+  const [data, editdata] = useState()
+
+  const docRef = doc(db,"Products","8SRuRjYdyCeTkoUqRgQn");
+  const docSnap =  getDoc(docRef).then(docSnap => {
+    editdata(docSnap.data())
+    console.log(data.glases)
+  })
 
   return (
     <div className="Body">
