54abbf6000a714826bcbb9d257e9dab5a025d2ab
Kaival <87368466+Kaival1212@users.noreply.github.com>
2023-03-15 17:11:57 +0000

    firestore api done

--- e-com-t1/src/App.js
+++ e-com-t1/src/App.js
@@ -1,73 +1,59 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Item from './Components/Items';
-import { useState } from 'react';
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
-import NavBarComponent from './Components/NavBar'
-import { doc, getDoc } from "firebase/firestore";
+import React, { useEffect } from 'react'; 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+import './App.css'; import Item from './Components/Items'; 
+import { useState } from 'react'; 
+import { initializeApp } from "firebase/app"; 
+import { getFirestore } from "firebase/firestore"; 
+import { getAnalytics } from "firebase/analytics"; 
+import NavBarComponent from './Components/NavBar' 
+import { doc, getDoc } from "firebase/firestore"; 
 
+const firebaseConfig = { 
+  apiKey: "AIzaSyDFNgbvMK16101ygeQksKe7YTw8Wc78bng", 
+  authDomain: "e-com-ti.firebaseapp.com", 
+  databaseURL: "https://e-com-ti-default-rtdb.europe-west1.firebasedatabase.app", 
+  projectId: "e-com-ti", storageBucket: "e-com-ti.appspot.com", 
+  messagingSenderId: "740384603849", 
+  appId: "1:740384603849:web:9d471bb5c5fa115a55d718", 
+  measurementId: "G-6K2MNFTDHS" 
+}; 
+  
+  const app = initializeApp(firebaseConfig); 
+  const analytics = getAnalytics(app); 
+  const db = getFirestore(app); 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDFNgbvMK16101ygeQksKe7YTw8Wc78bng",
-  authDomain: "e-com-ti.firebaseapp.com",
-  databaseURL: "https://e-com-ti-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "e-com-ti",
-  storageBucket: "e-com-ti.appspot.com",
-  messagingSenderId: "740384603849",
-  appId: "1:740384603849:web:9d471bb5c5fa115a55d718",
-  measurementId: "G-6K2MNFTDHS"
-};
+function App() { 
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore(app);
+    const [firestore_data , set_firestor_data] = useState() 
 
+    useEffect(()=>{ const docRef = doc(db,"Print Full Products","Product_list"); 
+    getDoc(docRef).then(docSnap => { set_firestor_data(docSnap.data().result) }) },[]) 
 
+    if (!firestore_data) 
+    { return <div>Loading...</div>; } 
 
-function App() {
-
-  return(
-    <BrowserRouter>
+    return( 
+    <BrowserRouter> 
       <Routes>
-        <Route index element={<LandingPage />} />
-        <Route path="Item_id_main_page" element={<NavBarComponent />} />
-        <Route path="*" element={<NavBarComponent />} />
+         <Route index element={<LandingPage data={firestore_data}/>} /> 
+         <Route path="Item_id_main_page" element={<NavBarComponent />} /> 
+         <Route path="*" element={<NavBarComponent />} /> 
       </Routes>
-    </BrowserRouter>
-  )
+    </BrowserRouter> ) 
 }
 
-export default App;
+export default App; 
+//khpaNGaiMdiwtWKQyHcIjq421UCMiw4qHJWYUYTb 
+//printfull 
 
-//eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIzN2Q0YmQzMDM1ZmUxMWU5YTgwM2FiN2VlYjNjY2M5NyIsImp0aSI6IjVlODk1ZWJmMGYyMzZkYTVlMmMzNDk5ZjQyYTlkYTcyY2ZhNWJjZDk2YmQ0MDU2MTFjMWEyN2UzY2VkZDZjMTk3MjY1YmFiNmY1YzU2NjZkIiwiaWF0IjoxNjc4NDg5NDQ2LjU1NjUxNCwibmJmIjoxNjc4NDg5NDQ2LjU1NjUxNywiZXhwIjoxNzEwMTExODQ2LjU1MjQ5LCJzdWIiOiIxMDQwNDU3NCIsInNjb3BlcyI6WyJzaG9wcy5tYW5hZ2UiLCJzaG9wcy5yZWFkIiwiY2F0YWxvZy5yZWFkIiwib3JkZXJzLnJlYWQiLCJvcmRlcnMud3JpdGUiLCJwcm9kdWN0cy5yZWFkIiwicHJvZHVjdHMud3JpdGUiLCJ3ZWJob29rcy5yZWFkIiwid2ViaG9va3Mud3JpdGUiLCJ1cGxvYWRzLnJlYWQiLCJ1cGxvYWRzLndyaXRlIiwicHJpbnRfcHJvdmlkZXJzLnJlYWQiXX0.AACc1ujbzOgNkXYy5ykKDyjEeOeyRImi-EF1C3TnCGIuqJcAPzL7d-rduS6vxw_QbqaHeK7xpyGtNCFGmz8
-//https://api.printify.com/v1/shops/7798398/products.json
-
-function LandingPage()
-{
-  const n = 15; 
-  const items = Array.from({ length: n }, (_, index) => (
-    <Item key={index} />
-  ));
-
-  const [data, editdata] = useState()
-
-  const docRef = doc(db,"Products","8SRuRjYdyCeTkoUqRgQn");
-  const docSnap =  getDoc(docRef).then(docSnap => {
-    editdata(docSnap.data())
-    console.log(data.glases)
-  })
-
-  return (
-    <div className="Body">
-      <NavBarComponent/>
-        <div className="Item-types">
-          Trending
-        </div>
-        <div className="Item-container">{items}</div>
-    </div>
-  );
-
+function LandingPage(props) 
+{ 
+  return ( 
+  <div className="Body"> <NavBarComponent/> 
+  <div className="Item-types"> Trending </div> 
+  <div className="Item-container">
+    {props.data.map(indi_product => <Item key={indi_product.id} data={indi_product}/>)}
+    
+  </div> 
+  </div> ); 
 }
--- e-com-t1/src/Components/Items.js
+++ e-com-t1/src/Components/Items.js
@@ -1,30 +1,21 @@
-import React from 'react'
-import './Items.css'
-import i from './Images/T-image.png'
+import React from 'react' 
+import './Items.css' 
 
-
-console.log(i); // /logo.84287d09.png
-
-function Item_view(props) {
-
-    return(
-        <div className="Item_view">
-
-            <img src={i} alt="Not Found" className='Item_Image'></img>
-            <a href="Item_id_main_page">
-            <p className='Item_name'>Letter And Bear Graphic Tee</p>
-            </a>
-            <div className='Price_view'>
-                <p className='Item_price'>GBP 10</p>
-                <div className='Content_btns'>
-                    <button className='Add_cart_btn hov'>Add to cart</button>
-                    <button className='Buy_btn hov'>Buy</button>
-                </div>
+function Item_view(props) 
+{ 
+    return( 
+    
+    <div className="Item_view"> 
+        <img src={props.data.thumbnail_url} alt="Not Found" className='Item_Image'></img> 
+        <a href="Item_id_main_page"> <p className='Item_name'>{props.data.name}</p> </a>
+            <div className='Price_view'> <p className='Item_price'>GBP 100</p> 
+                <div className='Content_btns'> 
+                <button className='Add_cart_btn hov'>Add to cart</button>
+                <button className='Buy_btn hov'>Buy</button> 
+                </div> 
             </div>
-
-        </div>
+    </div> 
     )
-
-}
+ } 
 
 export default Item_view
